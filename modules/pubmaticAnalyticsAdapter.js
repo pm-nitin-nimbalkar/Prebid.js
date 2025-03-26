ffe4ef2e51072b2b7b1c407808e45454af437219
Tanishka Vishwakarma <tanishkavishwakarma@L1837.local>
2025-03-26 16:52:28 +0530

    UOE-11861: FIXING ISSUES RELATED to meta and other keys

--- modules/pubmaticAnalyticsAdapter.js
+++ modules/pubmaticAnalyticsAdapter.js
@@ -35,24 +35,6 @@ const DEFAULT_PUBLISHER_ID = 0;
 const DEFAULT_PROFILE_ID = 0;
 const DEFAULT_PROFILE_VERSION_ID = 0;
 
-// TODO : Remove - Once BM calculation moves to Server Side
-
-const BROWSER_MAP = [
-
-  { value: /(firefox)\/([\w\.]+)/i, key: 12 }, // Firefox
-  { value: /\b(?:crios)\/([\w\.]+)/i, key: 1 }, // Chrome for iOS
-  { value: /edg(?:e|ios|a)?\/([\w\.]+)/i, key: 2 }, // Edge
-  { value: /(opera)(?:.+version\/|[\/ ]+)([\w\.]+)/i, key: 3 }, // Opera
-  { value: /(?:ms|\()(ie) ([\w\.]+)/i, key: 4 }, // Internet Explorer
-  { value: /fxios\/([-\w\.]+)/i, key: 5 }, // Firefox for iOS
-  { value: /((?:fban\/fbios|fb_iab\/fb4a)(?!.+fbav)|;fbav\/([\w\.]+);)/i, key: 6 }, // Facebook In-App Browser
-  { value: / wv\).+(chrome)\/([\w\.]+)/i, key: 7 }, // Chrome WebView
-  { value: /droid.+ version\/([\w\.]+)\b.+(?:mobile safari|safari)/i, key: 8 }, // Android Browser
-  { value: /(chrome|chromium|crios)\/v?([\w\.]+)/i, key: 9 }, // Chrome
-  { value: /version\/([\w\.\,]+) .*mobile\/\w+ (safari)/i, key: 10 }, // Safari Mobile
-  { value: /version\/([\w(\.|\,)]+) .*(mobile ?safari|safari)/i, key: 11 }, // Safari
-];
-
 /// /////////// VARIABLES //////////////
 let publisherId = DEFAULT_PUBLISHER_ID; // int: mandatory
 let profileId = DEFAULT_PROFILE_ID; // int: optional
@@ -60,16 +42,6 @@ let profileVersionId = DEFAULT_PROFILE_VERSION_ID; // int: optional
 let s2sBidders = [];
 
 /// /////////// HELPER FUNCTIONS //////////////
-
-// TODO : Remove - Once BM calculation moves to Server Side
-function getBrowserType() {
-  const userAgent = navigator?.userAgent;
-  let browserIndex = userAgent == null ? -1 : 0;
-  if (userAgent) {
-    browserIndex = BROWSER_MAP.find(({ value }) => value.test(userAgent))?.key || 0;
-  }
-  return browserIndex;
-}
 
 function formatSource(src = 'client') {
   return (src === 's2s' ? 'server' : src).toLowerCase();
@@ -81,6 +53,7 @@ function sendAjaxRequest({ endpoint, method, queryParams = '', body = null }) {
 };
 
 function copyRequiredBidDetails(bid) {
+
   return pick(bid, [
     'bidder',
     'bidderCode',
@@ -146,7 +119,7 @@ function parseBidResponse(bid) {
     'originalCurrency',
     'adserverTargeting',
     'dealChannel',
-    'meta',
+    'meta',() => (bid.meta && Object.keys(bid.meta).length > 0 ? bid.meta : undefined),
     'status',
     'error',
     'bidId',
@@ -221,11 +194,9 @@ function getRootLevelDetails(auctionCache, auctionId) {
     tst: Math.round(Date.now() / 1000),
     pid: `${profileId}`,
     pdvid: `${profileVersionId}`,
-    pbv: '$prebid.version$' || '-1',
     ortb2: auctionCache.ortb2,
     tgid: getTgId(),
     s2sls: s2sBidders,
-    bm: getBrowserType(),
     it: getIntegrationType(),
     dm: DISPLAY_MANAGER,
     dmv: '$prebid.version$' || '-1'
@@ -253,6 +224,8 @@ function executeBidsLoggerCall(event, highestCpmBids) {
         auctionCache.adUnitCodes[bid?.adUnitId].bidWonAdId = auctionCache.adUnitCodes[bid?.adUnitId].bidWonAdId ? auctionCache.adUnitCodes[bid?.adUnitId].bidWonAdId : winBid;
         bid.bidId = bidId;
         bid.mi = bid?.bidResponse ? bid.bidResponse.mi : (window.matchedimpressions && window.matchedimpressions[bid.bidder]);
+        let adapterName = getAdapterNameForAlias(bid.adapterCode || bid.bidder);
+        bid.bidder = adapterName;
       });
     });
   });
@@ -393,6 +366,7 @@ const eventHandlers = {
     bid.clientLatencyTimeMs = Date.now() - cache.auctions[args.auctionId].timestamp;
     bid.bidResponse = parseBidResponse(args);
     bid.bidderCode = args.bidderCode || bid.bidderCode;
+    bid.bidder = getAdapterNameForAlias(args.adapterCode || bid.bidderCode);
     bid.adapterName = getAdapterNameForAlias(args.adapterCode || bid.bidderCode);
   },
 
